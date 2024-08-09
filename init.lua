6ab0212314f9f8eb03b958dfb648bb455d37409c
Hicham Mallah <mallah.hicham@gmail.com>
2024-08-09 17:23:47 +0200

    testing(git output): testing what to do not to keep the terminal expanded when command is done

--- init.lua
+++ init.lua
@@ -99,7 +99,7 @@ local function run_git_commands(args)
     return
   end
   handle:close()
-  print(result:gsub('[\n\r]', ' '))
+  print(result)
 end
 vim.api.nvim_create_user_command('G', run_git_commands, { nargs = '?' })
 -- Set to true if you have a Nerd Font installed and selected in the terminal
